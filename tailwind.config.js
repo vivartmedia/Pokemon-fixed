34c103b7656cf8a72f7410f0a29f775c49d4e29f
vivartmedia <nanno70@yahoo.de>
2024-01-29 21:24:58 -0800

    Init Repo

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./*.{html,js}",
+    require('flowbite/plugin'),
+    "./node_modules/flowbite/**/*.js"
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
